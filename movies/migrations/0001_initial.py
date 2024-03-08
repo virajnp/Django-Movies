22dd4ee7b883d75164473b3eb05c02c7fe27564d
Viraj <virajnp@gmail.com>
2024-03-07 22:51:12 -0600

    Applying migrations

--- movies/migrations/0001_initial.py
+++ movies/migrations/0001_initial.py
@@ -15,19 +15,22 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Genre',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True,
+                 primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=255)),
             ],
         ),
         migrations.CreateModel(
             name='Movies',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True,
+                 primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.IntegerField()),
                 ('release_year', models.IntegerField()),
                 ('numbers_in_stock', models.IntegerField()),
                 ('daily_rate', models.FloatField()),
-                ('genre', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='movies.Genre')),
+                ('genre', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE, to='movies.Genre')),
             ],
         ),
     ]
